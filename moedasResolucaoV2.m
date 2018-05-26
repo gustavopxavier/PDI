2854bf85b8ca5b2f8107dd6d18186ed56c09ca81
gustavopxavier <gustavopxavier@gmail.com>
2018-05-25 21:26:03 -0300

    Rotulação

--- moedasResolucaoV2.m
+++ moedasResolucaoV2.m
@@ -93,25 +93,26 @@ imwrite(imLimiar,'/home/gustavo/projetosGit/PDI/imLimiar.jpg');
 %%%%%%%%%%%%%%%%% MOEDAS - EROSÃO %%%%%%%%%%%%%%%%%%%%%%%%%%%%%  
 A = imLimiar;
 
-B = [1 1 1; 1 1 1; 1 1 1];
-EE = [1 1 1 1 1; 1 1 1 1 1; 1 1 1 1 1; 1 1 1 1 1; 1 1 1 1 1];
-
-imMoedasOpen = imopen(A, EE);
-
-imwrite(imMoedasOpen,'/home/gustavo/projetosGit/PDI/imMoedasOpen.jpg');
-
-figure('Name','Imagem Moedas Abertura');
-imshow(imMoedasOpen);
+B = [1 1 1 1 1; 
+     1 1 1 1 1; 
+     1 1 1 1 1; 
+     1 1 1 1 1; 
+     1 1 1 1 1];
 
 C = A;
 
-for i=2:size(A,1)-1
-  for j=2:size(A,2)-1
+for i=2:size(A,1)-2
+  for j=2:size(A,2)-2
     if(A(i,j)==1) %se o pixel central da vizinhança de A = 1, deve ser analizado
-      vizA = [A(i-1,j-1) A(i-1,j) A(i-1,j+1);...
-            A(i,j-1) A(i,j) A(i,j+1);...
-            A(i+1,j-1) A(i+1,j) A(i+1,j+1)];
-      if (sum(sum(vizA==B))!=9) % se todos os pixels são iguais entre a vizinhança de A e B
+       vizA = [A(i-2,j-2) A(i-2,j-1) A(i-2,j) A(i-2,j+1) A(i-2,j+2);...
+             A(i-1,j-2) A(i-1,j-1) A(i-1,j) A(i-1,j+1) A(i-1,j+2);...
+             A(i,j-2) A(i,j-1) A(i,j) A(i,j+1) A(i,j+2);...
+             A(i+1,j-2) A(i+1,j-1) A(i+1,j) A(i+1,j+1) A(i+1,j+2);...
+             A(i+2,j-2) A(i+2,j-1) A(i+2,j) A(i+2,j+1) A(i+2,j+2)];
+%      vizA = [A(i-1,j-1) A(i-1,j) A(i-1,j+1);...
+%            A(i,j-1) A(i,j) A(i,j+1);...
+%            A(i+1,j-1) A(i+1,j) A(i+1,j+1)];
+      if (sum(sum(vizA==B))!=25) % se todos os pixels são iguais entre a vizinhança de A e B
         C(i,j)=0;
       end    
     end
@@ -122,3 +123,80 @@ figure('Name','Imagem Moedas Erodida')
 imshow(C)
 
 imwrite(C,'/home/gustavo/projetosGit/PDI/imMoedasErodidas.jpg');
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%   
+%%%%%%%%%%%%%%%%% MOEDAS - ROTULANDO %%%%%%%%%%%%%%%%%%%%%%%%%%%%%  
+imBW = C;
+
+rotulo = 2;
+rotulosIguais(1,1) = 0;
+%rotulosIguais(2,1) = 0;
+r = 1; %indice do vetor rotulos Iguais
+imRotulo = imBW; %imagem cópia para armazenar os rotulos
+for(i=2:size(imBW,1))
+  for(j=2:size(imBW,2))
+    if(imBW(i,j)==1)
+      if((imBW(i-1,j)==0)&&(imBW(i,j-1)==0))
+        rotulo+=20; % novo rotulo (soma de 10 em 10)
+        imRotulo(i,j)=rotulo;
+      else
+        if((imBW(i-1,j)==1)&&(imBW(i,j-1)==0))
+          imRotulo(i,j)=imRotulo(i-1,j);
+        else
+          if((imBW(i-1,j)==0)&&(imBW(i,j-1)==1))
+            imRotulo(i,j)=imRotulo(i,j-1);
+          else
+            if(((imBW(i-1,j)==1)&&(imBW(i,j-1)==1))&&(imRotulo(i-1,j)==imRotulo(i,j-1)))
+              imRotulo(i,j)=imRotulo(i-1,j);
+            else
+              if(((imBW(i-1,j)==1)&&(imBW(i,j-1)==1))&&(imRotulo(i-1,j)!=imRotulo(i,j-1)))
+                %os vizinhos são rotulados e os rotulos são diferentes...
+                imRotulo(i,j)=imRotulo(i-1,j); %insere o rotulo de um dos vizinhos
+                eq1 = imRotulo(i-1,j); 
+                eq2 = imRotulo(i,j-1);
+                % guardar rotulos equivalentes - erro
+                ultimaLinha = size(rotulosIguais,1);
+                ultimaColuna = size(rotulosIguais,2);
+                [l1,c1]=find(rotulosIguais==eq1); %busca o 1o elemento
+                [l2,c2]=find(rotulosIguais==eq2); %busca o 2o elemento
+                if ((isempty(l1))&&(isempty(l2))) %não achou nenhum dos elementos
+                  rotulosIguais(ultimaLinha+1,1) = eq1;
+                  rotulosIguais(ultimaLinha+1,2) = eq2;
+                else
+                  if ((!isempty(l1))&&(isempty(l2))) %não achou o 1o elemento
+                    rotulosIguais(l1,ultimaColuna+1) = eq2;
+                  else
+                    if ((isempty(l1))&&(!isempty(l2))) %não achou o 2o elemento
+                      rotulosIguais(l2,ultimaColuna+1) = eq1;
+                    end
+                  end
+                end
+              end  
+            end
+          end
+        end
+      end
+    end
+  end
+end
+
+figure('Name','Imagem Rotulada com erros')
+imshow(uint8(imRotulo), [min(min(imRotulo)), max(max(imRotulo))])
+
+%retirar erros de equivalências de rótulos
+for(i=2:size(imRotulo,1))
+  for(j=2:size(imRotulo,2))
+    if(imRotulo(i,j)!=0)
+      [l,c]=find(rotulosIguais==imRotulo(i,j)); %procura o elemento nos erros
+      if(!isempty(l))
+        imRotulo(i,j) = rotulosIguais(l,1);
+      end
+    end
+  end
+end
+
+qtdRegioes =  size(unique(imRotulo),1)-1; %-1 para desconsiderar o fundo
+imRotulo = uint8(imRotulo);
+figure('Name','Imagem Rotulada 2')
+imshow(imRotulo, [min(min(imRotulo)) max(max(imRotulo))])
+title(strcat('Quantidade de Moedas: ',int2str(qtdRegioes)))
